df3f7c6858e5787118c932ab0ee7bf352d56fdb3
laobubu <laobubu@gmail.com>
2016-02-21 21:03:50 +0800

    refine style

--- src/cs/script.js
+++ src/cs/script.js
@@ -43,15 +43,35 @@
 		var rects = range.getClientRects()
 		var rect = rects[rects.length - 1]
 
-		container.style.left = (rect.left + document.body.scrollLeft) + 'px'
-		container.style.top = (rect.top + document.body.scrollTop) + 'px'
+		var left = rect.left
+		var top = rect.top
+
+		if ((top - box.div.offsetHeight) < 0) {
+			box.div.classList.add('vertical-reverse')
+			top = rect.top + rect.height
+		} else {
+			box.div.classList.remove('vertical-reverse')
+		}
+
+		if ((left + box.div.offsetWidth) > window.innerWidth) {
+			left = window.innerWidth - box.div.offsetWidth
+		}
+
+		container.style.left = (left + document.body.scrollLeft) + 'px'
+		container.style.top = (top + document.body.scrollTop) + 'px'
 	}
 
 	function onSelectionChange() {
 		var range = selection.rangeCount && selection.getRangeAt(0)
-		
+
 		if (selection.isCollapsed) {
-			setBoxVisibility(range && (range.startContainer.childNodes[range.startOffset] === container) || false)
+			setBoxVisibility(
+				range && (
+					range.startContainer.childNodes[range.startOffset] === container ||
+					range.startContainer === box.div ||
+					range.startContainer.compareDocumentPosition(box.div) === Node.DOCUMENT_POSITION_CONTAINED_BY
+					) || false
+				)
 			return
 		}
 
@@ -61,7 +81,9 @@
 		tempDiv.appendChild(content)
 
 		var html = tempDiv.innerHTML
-		var text = tempDiv.innerText
+		var text = tempDiv.innerText.trim()
+
+		if (!text.length) return;
 
 		setBoxVisibility(true)
 		setBoxPosition()
@@ -69,7 +91,7 @@
 
 	document.addEventListener('selectionchange', onSelectionChange, true)
 	window.addEventListener('resize', setBoxPosition, false)
-	
+
 	setBoxVisibility(false)
 	window.tabox = box
 })();
